fed1e0cd070024a2cd6913c883914eccc3182bd4
MelodyParker <melodyechoparker@gmail.com>
2026-06-11 12:13:55 -0500

    fixing some async issues

--- effects_engine/Effect_Engine.py
+++ effects_engine/Effect_Engine.py
@@ -1,10 +1,13 @@
 import asyncio
 from Effect import Effect
+from typing import Optional, Dict
 
 class Effect_Engine:
     def __init__(self, pixels):
-        self.registered_effects = {}
+        self.registered_effects: Dict[str, type[Effect]] = {}
         self.pixels = pixels
+        self.active_task: Optional[asyncio.Task] = None
+
     def register_effect(self, cls, name):
         if issubclass(cls, Effect):
             self.registered_effects[name] = cls
@@ -16,9 +19,16 @@ class Effect_Engine:
         return decorator
     
     async def run_effect(self, name, *args, **kwargs):
-        if not self.registered_effects.get(name):
-            raise KeyError("Unregistered effect")
         effect = self.registered_effects.get(name)
-        await effect.run(self.pixels, *args, **kwargs)
+        if effect is None:
+            raise KeyError("Unregistered effect")
+        if self.active_task and not self.active_task.done():
+            print("Cancelling ongoing effect")
+            self.active_task.cancel()
+            try:
+                await self.active_task
+            except asyncio.CancelledError:
+                print("Old effect killed successfully")
+        self.active_task = asyncio.create_task(effect.run(self.pixels, *args, **kwargs))
         
     
