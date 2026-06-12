fd68b3ae0c622cceb3290ec7d7178b375e5d7a11
MelodyParker <melodyechoparker@gmail.com>
2026-06-12 09:49:30 -0500

    testing limits of my system

--- effects.py
+++ effects.py
@@ -3,7 +3,7 @@ import neopixel
 import asyncio
 from effects_engine.Effect import Effect
 from effects_engine.Effects_Engine import Effects_Engine
-
+import math
 
 PIXEL_PIN = board.D18
 NUM_PIXELS = 50
@@ -24,6 +24,8 @@ def gamma_correct(color):
     
     return (r_corr, g_corr, b_corr)
 
+def lerp(low, high, prop):
+    return low + (high - low) * prop 
 
 
 pixels = neopixel.NeoPixel(
@@ -104,15 +106,27 @@ class AlternatingColorsEffect(Effect):
             offset -= direction
             await asyncio.sleep(duration)
 
-        
-# class GradientEffect(Effect):
-#     @staticmethod
-#     async def run(pixels, colors, move=False, direction=1, duration=1):
-#         num_colors = len(colors)
-#         num_pixels = len(pixels)
-#         if not move:
-#             for i, pixel in enumerate(pixels):
-#                 num = (i / num_pixels) 
+@engine.register_effect_factory("gradient", name="Gradient", description="Gradient between given colors", params=[
+    {"type": "list color", "length": "N", "name": "Colors"}
+])
+class GradientEffect(Effect):
+    @staticmethod
+    async def run(pixels, colors, move=False, direction=1, duration=1):
+        num_colors = len(colors)
+        num_pixels = len(pixels)
+        if not move:
+            for i, pixel in enumerate(pixels):
+                num = (i / num_pixels) * (num_colors - 1)
+                color_1 = math.floor(num)
+                color_2 = math.ceil(num)
+                frac = num - color_1
+                r1, g1, b1 = colors[color_1]
+                r2, g2, b2 = colors[color_2]
+                rg = lerp(r1, r2, frac)
+                gg = lerp(g1, g2, frac)
+                bg = lerp(b1, b2, frac)
+                pixels[i] = gamma_correct((rg, gg, bg))
+
                 
 
 
@@ -124,7 +138,7 @@ async def main():
         while True:
             # await engine.run_effect("alternating-colors", [(228, 3, 3), (255, 140, 0), (255, 237, 0), (0, 128, 38), (0, 76, 255), (115, 41, 130)], True, 2, 1)
             # await engine.run_effect("fill-rgb", rgb=(255, 255, 255), pixels=pixels[::2])
-            await engine.run_effect("flash-colors", colors=[(255, 0, 0), (0, 255, 0)], durations=[1, 1])
+            await engine.run_effect("gradient", colors=[(255, 0, 0), (0, 255, 0)])
             await asyncio.sleep(float('inf'))
             # await engine.run_effect("flash-colors", [(255, 0, 0), (0, 255, 0), (0, 0, 255)], [0.3, 0.3, 0.4]) # make it green?
             # await asyncio.sleep(3)
